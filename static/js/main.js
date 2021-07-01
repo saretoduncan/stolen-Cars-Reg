cb30b7861ebdfaca54ef51f2f38206cb839b2521
sareto <duncan.moiyo@student.moringaschool.com>
2021-07-02 02:48:13 +0300

    add more function

--- static/js/main.js
+++ static/js/main.js
@@ -116,56 +116,56 @@ if (newLocalArr !== null) {
 }
 const view = document.querySelectorAll(".view");
 view.forEach(item => { ///pop-up opening
-  item.addEventListener('click', e => {
-    let target = e.target;
-    let parentDiv = target.parentElement.parentElement
-    for (let i = 0; i < newLocalArr.length; i++) {
-      if (parentDiv.querySelector("#plate").innerText === newLocalArr[i].licence) {
-        document.querySelector("#carMake").innerHTML = newLocalArr[i].make;
-        document.querySelector("#carType").innerHTML = newLocalArr[i].vehicleType;
-        document.querySelector("#carExterior").innerHTML = newLocalArr[i].exterior;
-        document.querySelector("#carInterior").innerHTML = newLocalArr[i].interior;
-        document.querySelector("#numberPlate").innerHTML = newLocalArr[i].licence;
-        document.querySelector("#chassis_Number").innerHTML = newLocalArr[i].chassis;
-        const imageDaraUrl = localStorage.getItem(newLocalArr[i].image);
+    item.addEventListener('click', e => {
+      let target = e.target;
+      let parentDiv = target.parentElement.parentElement
+      for (let i = 0; i < newLocalArr.length; i++) {
+        if (parentDiv.querySelector("#plate").innerText === newLocalArr[i].licence) {
+          document.querySelector("#carMake").innerHTML = newLocalArr[i].make;
+          document.querySelector("#carType").innerHTML = newLocalArr[i].vehicleType;
+          document.querySelector("#carExterior").innerHTML = newLocalArr[i].exterior;
+          document.querySelector("#carInterior").innerHTML = newLocalArr[i].interior;
+          document.querySelector("#numberPlate").innerHTML = newLocalArr[i].licence;
+          document.querySelector("#chassis_Number").innerHTML = newLocalArr[i].chassis;
+          const imageDaraUrl = localStorage.getItem(newLocalArr[i].image);
 
-        if (imageDaraUrl) {
-          document.querySelector("#carImg").setAttribute('src', imageDaraUrl);
+          if (imageDaraUrl) {
+            document.querySelector("#carImg").setAttribute('src', imageDaraUrl);
+          }
         }
       }
-    }
 
-    pop.classList.add("flx");
+      pop.classList.add("flx");
 
 
+    })
   })
-})
-const searchBox = document.querySelector("#searchbox").value.toUpperCase();
-const searchBtn = document.querySelector("#searchBx")
-searchBtn.addEventListener("click", () => {
+  // const searchBox = document.querySelector("#searchbox").value.toUpperCase();
+  // const searchBtn = document.querySelector("#searchBx")
+  // searchBtn.addEventListener("click", () => {
 
-  for (let i = 0; i < newLocalArr.length; i++) {
-    if (searchBox === newLocalArr[i].chassis) {
+//   for (let i = 0; i < newLocalArr.length; i++) {
+//     if (searchBox === newLocalArr[i].chassis) {
 
-      document.querySelector("#carMake").innerHTML = newLocalArr[i].make;
-      document.querySelector("#carType").innerHTML = newLocalArr[i].vehicleType;
-      document.querySelector("#carExterior").innerHTML = newLocalArr[i].exterior;
-      document.querySelector("#carInterior").innerHTML = newLocalArr[i].interior;
-      document.querySelector("#numberPlate").innerHTML = newLocalArr[i].licence;
-      document.querySelector("#chassis_Number").innerHTML = newLocalArr[i].chassis;
-      const imageDaraUrl = localStorage.getItem(newLocalArr[i].image);
+//       document.querySelector("#carMake").innerHTML = newLocalArr[i].make;
+//       document.querySelector("#carType").innerHTML = newLocalArr[i].vehicleType;
+//       document.querySelector("#carExterior").innerHTML = newLocalArr[i].exterior;
+//       document.querySelector("#carInterior").innerHTML = newLocalArr[i].interior;
+//       document.querySelector("#numberPlate").innerHTML = newLocalArr[i].licence;
+//       document.querySelector("#chassis_Number").innerHTML = newLocalArr[i].chassis;
+//       const imageDaraUrl = localStorage.getItem(newLocalArr[i].image);
 
-      if (imageDaraUrl) {
-        document.querySelector("#carImg").setAttribute('src', imageDaraUrl);
-      }
+//       if (imageDaraUrl) {
+//         document.querySelector("#carImg").setAttribute('src', imageDaraUrl);
+//       }
 
-    }
+//     }
 
-    pop.classList.add("flx");
-  }
-});
+//     pop.classList.add("flx");
+//   }
+// });
 
-
+//pop-up closing;
 reportBtn.addEventListener('click', () => {
   document.querySelector(".reportTop").classList.add("flx");
 });
