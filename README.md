b157b869af549e961b5bfcdbddccba73b73a0d7c
sareto <duncan.moiyo@student.moringaschool.com>
2021-07-02 09:23:09 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# {Dancan Moiyo, Mourine Kitili ,Caroline W. Nyambura }
+## {StolenCar Reg}, {2021}
+## Description
+{A website that gives users  a way to identify a stolen vehicle before purchase,a site to post their stolen vehicle and prevent someone from falling victim to criminals and a place to start looking when they are trying to find their stolen vehicle or that of family and friends.Its a Website you can easily navigate though on you phone,tablet,laptop or desktop computer:No installation whatsoever required.
+}
+## Technologies Used
+{HTML,CSS AND JAVASCRIPT}
+## Support and Contact Details
+{For enquiries feel free to contact us : email - Contact us at stolencarreg@gmail.com or Tel:+2457 123 456 78}
+## Github Pages
+Copyright (c) {2021}
+## Licence
+MIT License
+Copyright (c) [2021] [StolenCar Reg]
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this website and associated documentation files (the "StolenCar Reg"), to deal
+in the Website without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Website, and to permit persons to whom the Website is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Website.
+THE WEBSITE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE WEBSITE OR THE USE OR OTHER DEALINGS IN THE
+WEBSITE.
--- static/js/main.js
+++ static/js/main.js
@@ -7,10 +7,11 @@ const registrationBtn = document.querySelector("#regBtn");
 const imgUpload = document.querySelector("#uploadimage");
 
 
-
-rapoClose.addEventListener('click', () => {
-  document.querySelector(".reportTop").classList.remove("flx");
-})
+if (rapoClose) {
+  rapoClose.addEventListener('click', () => {
+    document.querySelector(".reportTop").classList.remove("flx");
+  })
+}
 class NewCar {
   constructor(image, make, model, year, exterior, interior, licence, chassis, vehicleType) {
     this.image = image;
@@ -108,64 +109,71 @@ if (newLocalArr !== null) {
     parentDiv.setAttribute("class", "container-fluid row viewbox");
     parentDiv.innerHTML = `<div class="d-flex align-items-center col-12 col-md-3"><p class="text-light fs-4" id="carTp">${item.make}</p></div><div class="d-flex align-items-center col-12 col-md-3"><p class="text-light fs-4" id="datte">${item.year}</p></div><div class="d-flex align-items-center col-12 col-md-3"><p class="text-light fs-4" id="plate">${item.licence}</p></div><div class="d-md-flex align-items-center justify-content-center col-12 col-md-3"><button class="btn btn-primary view">VIEW</button></div>`
     parReg.appendChild(parentDiv);
-    document.querySelector('.stolenReg').appendChild(parReg);
-
-
-
+    if (document.querySelector('.stolenReg')) {
+      document.querySelector('.stolenReg').appendChild(parReg);
+    }
   })
 }
 const view = document.querySelectorAll(".view");
 view.forEach(item => { ///pop-up opening
-    item.addEventListener('click', e => {
-      let target = e.target;
-      let parentDiv = target.parentElement.parentElement
-      for (let i = 0; i < newLocalArr.length; i++) {
-        if (parentDiv.querySelector("#plate").innerText === newLocalArr[i].licence) {
-          document.querySelector("#carMake").innerHTML = newLocalArr[i].make;
-          document.querySelector("#carType").innerHTML = newLocalArr[i].vehicleType;
-          document.querySelector("#carExterior").innerHTML = newLocalArr[i].exterior;
-          document.querySelector("#carInterior").innerHTML = newLocalArr[i].interior;
-          document.querySelector("#numberPlate").innerHTML = newLocalArr[i].licence;
-          document.querySelector("#chassis_Number").innerHTML = newLocalArr[i].chassis;
-          const imageDaraUrl = localStorage.getItem(newLocalArr[i].image);
+  item.addEventListener('click', e => {
+    let target = e.target;
+    let parentDiv = target.parentElement.parentElement
+    for (let i = 0; i < newLocalArr.length; i++) {
+      if (parentDiv.querySelector("#plate").innerText === newLocalArr[i].licence) {
+        document.querySelector("#carMake").innerHTML = newLocalArr[i].make;
+        document.querySelector("#carType").innerHTML = newLocalArr[i].vehicleType;
+        document.querySelector("#carExterior").innerHTML = newLocalArr[i].exterior;
+        document.querySelector("#carInterior").innerHTML = newLocalArr[i].interior;
+        document.querySelector("#numberPlate").innerHTML = newLocalArr[i].licence;
+        document.querySelector("#chassis_Number").innerHTML = newLocalArr[i].chassis;
+        const imageDaraUrl = localStorage.getItem(newLocalArr[i].image);
 
-          if (imageDaraUrl) {
-            document.querySelector("#carImg").setAttribute('src', imageDaraUrl);
-          }
+        if (imageDaraUrl) {
+          document.querySelector("#carImg").setAttribute('src', imageDaraUrl);
         }
+      }
+    }
+
+    pop.classList.add("flx");
+
+
+  })
+})
+
+
+const searchBtn = document.querySelector(".searchBx")
+  // searchBtn.addEventListener('click', function(e) {
+
+//   
+//   console.log(searchBox);
+// })
+if (searchBtn) {
+  searchBtn.addEventListener("click", () => {
+    let searchBox = document.querySelector(".searchboxx").value;
+    for (let i = 0; i < newLocalArr.length; i++) {
+      if (searchBox === newLocalArr[i].chassis) {
+
+        document.querySelector("#carMake1").innerHTML = newLocalArr[i].make;
+        document.querySelector("#carType1").innerHTML = newLocalArr[i].vehicleType;
+        document.querySelector("#carExterior1").innerHTML = newLocalArr[i].exterior;
+        document.querySelector("#carInterior1").innerHTML = newLocalArr[i].interior;
+        document.querySelector("#numberPlate1").innerHTML = newLocalArr[i].licence;
+        document.querySelector("#chassis_Number1").innerHTML = newLocalArr[i].chassis;
+        const imageDaraUrl = localStorage.getItem(newLocalArr[i].image);
+
+        if (imageDaraUrl) {
+          document.querySelector("#carImg").setAttribute('src', imageDaraUrl);
+        }
+
       }
 
       pop.classList.add("flx");
-
-
-    })
-  })
-  // const searchBox = document.querySelector("#searchbox").value.toUpperCase();
-  // const searchBtn = document.querySelector("#searchBx")
-  // searchBtn.addEventListener("click", () => {
-
-//   for (let i = 0; i < newLocalArr.length; i++) {
-//     if (searchBox === newLocalArr[i].chassis) {
-
-//       document.querySelector("#carMake").innerHTML = newLocalArr[i].make;
-//       document.querySelector("#carType").innerHTML = newLocalArr[i].vehicleType;
-//       document.querySelector("#carExterior").innerHTML = newLocalArr[i].exterior;
-//       document.querySelector("#carInterior").innerHTML = newLocalArr[i].interior;
-//       document.querySelector("#numberPlate").innerHTML = newLocalArr[i].licence;
-//       document.querySelector("#chassis_Number").innerHTML = newLocalArr[i].chassis;
-//       const imageDaraUrl = localStorage.getItem(newLocalArr[i].image);
-
-//       if (imageDaraUrl) {
-//         document.querySelector("#carImg").setAttribute('src', imageDaraUrl);
-//       }
-
-//     }
-
-//     pop.classList.add("flx");
-//   }
-// });
-
-//pop-up closing;
-reportBtn.addEventListener('click', () => {
-  document.querySelector(".reportTop").classList.add("flx");
-});
+    }
+  });
+}
+if (reportBtn) {
+  reportBtn.addEventListener('click', () => {
+    document.querySelector(".reportTop").classList.add("flx");
+  });
+}
